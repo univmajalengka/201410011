aa83ebe2cdca81e689fae8e48d192eaa704267d2
ilvalaelasari <ilvalaila44@gmail.com>
2020-12-08 11:14:54 +0700

    tugas 6

---
+++ program billing.cpp
@@ -0,0 +1,34 @@
+//Membuat program billing
+#include<iostream>
+using namespace std;
+int main()
+{
+	int td1,j1,m1,d1,sm1;
+	cout<<"Masukan Jam Awal :";
+	cin>>j1;
+	cout<<"Masukan Menit Awal :";
+	cin>>m1;
+	cout<<"Masukan Detik Awal :";
+	cin>>d1;
+	td1=(j1*3600)+(m1*60)+d1;
+	
+	int td2,j2,m2,d2,sm2;
+	cout<<"Masukan Jam Akhir :";
+	cin>>j2;
+	cout<<"Masukan Menit Akhir :";
+	cin>>m2;
+	cout<<"Masukan Detik Akhir :";
+	cin>>d2;
+	td2=(j2*3600)+(m2*60)+d2;
+	
+	int td3,j3,m3,d3,sm3;
+	cin>>td3;
+	td3=td2-td1;
+	j3=td3/3600;
+	sm3=td3%3600;
+	m3=sm3/60;
+	d3=sm3%60;
+	cout<<"HASIL PERBEDAAN ANTARA JAM AWAL & JAM AKHIR : "<<j3<<":"<<m3<<":"<<d3;
+	return 0;
+}
+	
